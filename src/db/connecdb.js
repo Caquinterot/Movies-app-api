6e45d322aceff721ca84f230aa91f57da9da1550
cesarQuintero <131917778+Caquinterot@users.noreply.github.com>
2025-11-06 16:32:59 -0500

    modificacion del archivo connecdb

--- src/db/connecdb.js
+++ src/db/connecdb.js
@@ -1,8 +1,27 @@
 import { Sequelize } from 'sequelize';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres',
 	logging: false,
+	dialectOptions: isProd
+		? {
+				ssl: {
+					require: true,
+					rejectUnauthorized: false,
+				},
+		  }
+		: {},
 });
 
 export default sequelize;
+
+//import { Sequelize } from 'sequelize';
+
+//const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//dialect: 'postgres',
+//logging: false,
+//});
+
+//export default sequelize;
